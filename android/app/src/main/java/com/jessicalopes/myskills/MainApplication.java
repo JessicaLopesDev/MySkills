5dfcde7d8e8680a90cee67b379e71bb4257f6644
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-01-24 20:18:34 -0300

    publishing app

--- android/app/src/main/java/com/jessicalopes/myskills/MainApplication.java
+++ android/app/src/main/java/com/jessicalopes/myskills/MainApplication.java
@@ -1,9 +1,11 @@
-package com.myskills;
+package com.jessicalopes.myskills;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -62,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.myskills.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.jessicalopes.myskills.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
